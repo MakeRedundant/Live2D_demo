6cbea5f36b9bc822e0dd136b552ba263f2137313
MakeRedundant <105352286+MakeRedundant@users.noreply.github.com>
2023-11-02 22:29:52 +1100

    added rem remove sound

--- waifu-tips.js
+++ waifu-tips.js
@@ -88,6 +88,11 @@ const live2d_models = [
         message: 'SDK4 official sample Rice <a href="https://www.live2d.com/eula/live2d-free-material-license-agreement_en.html">LICENSE</a>',
         version: 3
     },
+    {
+        name: 'remu',
+        message: 'SDK4 official sample Rice <a href="https://www.live2d.com/eula/live2d-free-material-license-agreement_en.html">LICENSE</a>',
+        version: 3
+    },
 ]
 /****************************************************************************************************/
 // SessionStorage LocalStorage
